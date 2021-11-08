1a94ea2ead40ee37591b36233d72e336f7f924a6
eito-fis <eamon@itofisher.com>
2021-11-07 20:19:18 -0500

    ADD: Work on hw 7

--- hws/7/alu.sv
+++ hws/7/alu.sv
@@ -17,5 +17,39 @@ output logic equal; // is high if a == b.
 // Use *only* structural logic and previously defined modules to implement an 
 // ALU that can do all of operations defined in alu_types.sv's alu_op_code_t!
 
+/* ALU_AND  = 4'b0001, */
+/* ALU_OR   = 4'b0010, */
+/* ALU_XOR  = 4'b0011, */
+/* ALU_SLL  = 4'b0101, */
+/* ALU_SRL  = 4'b0110, */
+/* ALU_SRA  = 4'b0111, */
+/* ALU_ADD  = 4'b1000, */
+/* ALU_SUB  = 4'b1100, */
+/* ALU_SLT  = 4'b1101, */
+/* ALU_SLTU = 4'b1111 */
+
+logic [N-1:0] and_out;
+logic [N-1:0] or_out;
+logic [N-1:0] xor_out;
+logic [N-1:0] sll_out;
+logic [N-1:0] srl_out;
+logic [N-1:0] sra_out;
+logic [N-1:0] add_out;
+logic add_carry;
+logic [N-1:0] sub_out;
+logic sub_carry;
+logic [N-1:0] slt_out;
+logic [N-1:0] sltu_out;
+
+always_comb begin
+	and_out = a & b;
+	or_out = a | b;
+	xor_out = a ^ b;
+end
+
+adder_n #(.N(N)) ADD (.a(a), .b(b), .c_in(1'b0), .sum(add_out), .c_out(add_carry));
+adder_n #(.N(N)) SUB (.a(a), .b(~b), .c_in(1'b1), .sum(sub_out), .c_out(sub_carry));
+
+
 
 endmodule
--- hws/7/slt.sv
+++ hws/7/slt.sv
@@ -8,7 +8,23 @@ output logic out;
 
 // Copy any other modules you use into this folder and update the Makefile accordingly.
 
+logic [N-1:0] sub_out;
+logic [N-1:0] inv_b;
+logic sub_cout;
+logic is_overflow;
+logic final_bit;
+
+adder_n #(.N(N)) adder_32bit_a (
+	.a(a), .b(inv_b), .cin(1'b1), .out(sub_out), .cout(sub_cout)
+);
+
+always_comb begin
+	inv_b = ~b;
+	final_bit = sub_out[N - 1];
+	is_overflow = 
+		(~a[N - 1] & ~inv_b[N - 1] & sub_out[N - 1]) |
+		(a[N - 1] & inv_b[N - 1] & ~sub_out[N - 1]);
+	out = is_overflow ? ~final_bit : final_bit;
+end
 
 endmodule
-
-
