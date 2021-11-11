f2167ca9957e7f9f230a5b94d355f360caf84de7
eito-fis <eamon@itofisher.com>
2021-11-11 16:24:18 -0500

    ADD: Change pattern

--- 02_etch_a_sketch-part3/ili9341_display_controller.sv
+++ 02_etch_a_sketch-part3/ili9341_display_controller.sv
@@ -134,7 +134,13 @@ always_comb begin  : test_pattern
     pixel_color = WHITE;
   end
   else begin
-    case({pixel_x[4], pixel_y[4]})
+    /* case({pixel_x[4], pixel_y[4]}) */
+    /*   2'b00: pixel_color = RED; */
+    /*   2'b01: pixel_color = GREEN; */
+    /*   2'b10: pixel_color = BLUE; */
+    /*   2'b11: pixel_color = BLACK; */
+    /* endcase */
+    case(pixel_x[2:1])
       2'b00: pixel_color = RED;
       2'b01: pixel_color = GREEN;
       2'b10: pixel_color = BLUE;
